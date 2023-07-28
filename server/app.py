d1c1ca069935a2a95f83f177d00dda8afd99bada
richard miruka onsare <richardonsare15@gmail.com>
2023-07-28 15:52:55 +0300

    changed files

---
+++ server/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from server.models import db, migrate
+from server.routes import product_routes
+from server.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(product_routes)
+
+    return app
---
+++ server/config.py
@@ -0,0 +1,11 @@
+import cloudinary
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://richard:PHnUn9tYFVnLaIiw0dugeXqiYUIrWSdB@dpg-cie29q59aq0ce39k53sg-a.ohio-postgres.render.com/bird_app_h6ml'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    cloudinary.config(
+        cloud_name='dlgspxpwr',
+        api_key='162229873424843',
+        api_secret='PK64yfEbGdYzy7syywRCAhLOYGc'
+    )
