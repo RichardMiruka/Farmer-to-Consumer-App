8ed01421dc4e39e8995e7071e206ec7f7b97b82d
richard miruka onsare <richardonsare15@gmail.com>
2023-07-29 21:41:49 +0300

    changed files

--- server/config.py
+++ server/config.py
@@ -1,7 +1,8 @@
 import cloudinary
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://richard:PHnUn9tYFVnLaIiw0dugeXqiYUIrWSdB@dpg-cie29q59aq0ce39k53sg-a.ohio-postgres.render.com/bird_app_h6ml'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://richard:PHnUn9tYFVnLaIiw0dugeXqiYUIrWSdB@dpg-cie29q59aq0ce39k53sg-a.ohio-postgres.render.com/bird_app_h6ml'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@192.168.100.16/farmer-to-consumer-app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     cloudinary.config(
@@ -9,3 +10,4 @@ class Config:
         api_key='162229873424843',
         api_secret='PK64yfEbGdYzy7syywRCAhLOYGc'
     )
+    
