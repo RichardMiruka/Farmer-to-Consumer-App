5fd27bb9980521a80ebb6974242d4ad948724ca4
richard miruka onsare <richardonsare15@gmail.com>
2023-08-01 16:31:00 +0300

    a few changes

--- server/app.py
+++ server/app.py
@@ -3,11 +3,17 @@
 from flask import Flask
 from server.models import db, migrate
 from server.routes import product_routes
+from flask_jwt_extended import JWTManager
 from server.config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Set up JWT
+    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Replace with a long random string
+    jwt = JWTManager(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
