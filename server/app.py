749c2c6946197d91e420949c9cc4912bcd8dc254
richard miruka onsare <richardonsare15@gmail.com>
2023-08-01 16:44:44 +0300

    resolved conflict

--- server/app.py
+++ server/app.py
@@ -11,13 +11,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    # we have to replace this with our actual secret key in production
-    app.config['SECRET_KEY'] = 'your_flask_secret_key'
-
-    # The we generate a random JWT secret key and set it in the app's configuration
-    jwt_secret_key = secrets.token_hex(32)
-    app.config['JWT_SECRET_KEY'] = jwt_secret_key
-
     db.init_app(app)
     migrate.init_app(app, db)
 
